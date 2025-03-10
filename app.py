095e65124c64cd13906b906d998f682a421c02cc
OsamahBinSALMAN <51197969+OsamahBinSALMAN@users.noreply.github.com>
2025-03-10 09:27:38 +0300

    app.py

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS 
 import joblib
 import numpy as np
-
+import warnings
+ 
+warnings.filterwarnings('ignore')
 app = Flask(__name__)
+CORS(app)
 
 # Load trained models
 models = {
